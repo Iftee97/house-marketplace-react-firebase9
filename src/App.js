e041c7f84a66d271b643a8922841815a9888e313
Iftee97 <tawsifmasrur@gmail.com>
2022-10-01 16:30:26 +0600

    made Profile page to be a private route, cannot be accessed unless logged in

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import 'react-toastify/dist/ReactToastify.css'
 import Navbar from './components/Navbar'
 import Explore from './pages/Explore'
 import Offers from './pages/Offers'
+import PrivateRoute from './components/PrivateRoute'
 import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
@@ -19,7 +20,9 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
-          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile' element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />} />
+          </Route>
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
