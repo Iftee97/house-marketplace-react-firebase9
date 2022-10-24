269a24fcbc7f25c226d6555f566105ce8d2a304f
Iftee97 <tawsifmasrur@gmail.com>
2022-10-24 17:01:45 +0600

    added Offers page

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { db } from '../firebase.config'
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from 'firebase/firestore'
+import { toast } from 'react-toastify'
+import ListingItem from '../components/ListingItem'
 
 const Offers = () => {
+  const [listings, setListings] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const params = useParams()
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        // get reference
+        const listingsRef = collection(db, 'listings')
+
+        // create a query
+        const q = query(
+          listingsRef,
+          where('offer', '==', true),
+          orderBy('timestamp', 'desc'),
+          limit(10)
+        )
+
+        // execute query
+        const querySnap = await getDocs(q)
+
+        const listings = []
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          })
+        })
+
+        setListings(listings)
+        setLoading(false)
+      } catch (error) {
+        console.log(error)
+        toast.error('could not fetch listings')
+      }
+    }
+    fetchListings()
+  }, [])
+
   return (
-    <div>
-      <h1>offers</h1>
+    <div className='category'>
+      <header>
+        <p className="pageHeader">Offers</p>
+      </header>
+      {loading ? (
+        <p>loading...</p>
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className='categoryListings'>
+              {listings.map((listing) => (
+                <ListingItem
+                  listing={listing.data}
+                  id={listing.id}
+                  key={listing.id}
+                />
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : <p>No listings for {params.categoryName}</p>}
     </div>
   )
 }
