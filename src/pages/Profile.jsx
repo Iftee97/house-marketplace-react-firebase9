99aa74f986163883499cc4a4e0405fd9b3df2469
Iftee97 <tawsifmasrur@gmail.com>
2022-10-31 17:16:17 +0600

    added user listings to Profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,30 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-// import ListingItem from '../components/ListingItem'
+import ListingItem from '../components/ListingItem'
 import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
 import homeIcon from '../assets/svg/homeIcon.svg'
+import { toast } from 'react-toastify'
 
 // firebase imports
-import { auth } from '../firebase.config.js'
+import { db, auth } from '../firebase.config.js'
 import { signOut } from 'firebase/auth'
+import {
+  collection, getDocs, query, where, orderBy, doc, deleteDoc
+} from 'firebase/firestore'
 
 // custom hook
 import { useAuthContext } from '../hooks/useAuthContext'
 
 const Profile = () => {
-  const nagivate = useNavigate()
+  const [loading, setLoading] = useState(true)
+  const [listings, setListings] = useState(null)
+  const navigate = useNavigate()
   const { dispatch } = useAuthContext()
+
+  useEffect(() => {
+    const fetchUserListings = async () => {
+      setLoading(true)
+      const listingsRef = collection(db, 'listings') // create ref
+
+      const q = query(
+        listingsRef,
+        where('userRef', '==', auth.currentUser.uid),
+        orderBy('timestamp', 'desc')
+      ) // create query
+
+      const querySnap = await getDocs(q) // get query snapshot
+
+      let listings = []
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        })
+      })
+
+      setListings(listings)
+      setLoading(false)
+    }
+    fetchUserListings()
+  }, [])
 
   const handleClick = async () => {
     await signOut(auth)
-
-    // dispatch LOGOUT action
-    dispatch({ type: "LOGOUT" })
-
-    nagivate('/sign-in') // redirect to sign in page
+    dispatch({ type: "LOGOUT" }) // dispatch LOGOUT action
+    navigate('/sign-in') // redirect to sign in page
   }
 
-  console.log("auth.currentUser.displayName:", auth.currentUser.displayName)
+  // console.log("auth.currentUser.displayName:", auth.currentUser.displayName)
+
+  const onDelete = async (id) => {
+    if (window.confirm('Are you sure you want to delete this listing?')) {
+      const listingRef = doc(db, 'listings', id)
+      await deleteDoc(listingRef)
+
+      const updatedListings = listings.filter((listing) => listing.id !== id)
+      setListings(updatedListings)
+      toast.success('Successfully deleted listing')
+    }
+  }
+
+  const onEdit = (id) => {
+    navigate(`/edit-listing/${id}`)
+  }
 
   return (
     <div className='profile'>
@@ -60,6 +105,23 @@ const Profile = () => {
           <p>Sell or rent your home</p>
           <img src={arrowRight} alt="arrow right" />
         </Link>
+
+        {!loading && listings.length > 0 && (
+          <>
+            <p className="listingText">Your listings</p>
+            <ul className="listingsList">
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                  onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
+                />
+              ))}
+            </ul>
+          </>
+        )}
       </main>
     </div>
   )
